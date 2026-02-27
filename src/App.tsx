921050c0f5b9022692593a8ae08d2db2c6b516a7
manoj30dec <manoj.kumar@nagarro.com>
2026-02-27 12:57:45 +0530

    moved routing in app and added home component

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,50 @@
 import './App.css';
-import Header from './components/Header/Header';
-// import Sidebar from './components/Sidebar/Sidebar';
+import { Route, Routes } from 'react-router-dom';
+import Home from './components/Home/Home';
+import DashBoard from './components/DashBoard/DashBoard';
+import EditTask from './components/EditTask/EditTask';
+import TaskDetail from './components/TaskDetail/TaskDetail';
+import UseReducerExample from './components/UseReducerExample/UseReducerExample';
+import WindowDimensions from './components/WindowDimensions/WindowDimensions';
+import SendParent from './components/SendParent/SendParent';
+import SimpleUpdate from './components/SimpleUpdate/SimpleUpdate';
+import Search from './components/Search/Search';
+import BuggyComponent from './components/BuggyComponent/BuggyComponent';
+import ErrorBoundary from './components/ErrorBoundary/ErrorBoundary';
+import DemoCounter from './components/DemoCounter/DemoCounter';
 
 
-import { Outlet } from 'react-router-dom';
 function App() {
-  
-
+  const getTitle = (obj: any) => {
+  return obj;
+};
   return (
-    <div className="block">
-      <Header />
-      <div id="mainwrapper" className="flex w-full">
-        <div id="maincontent" className="p-4 w-full">
-          <Outlet/>
-        </div>
-      </div>
-    </div>
+    <>
+        <Routes>
+          <Route path="/" element={<Home />}>
+            <Route index element={<DashBoard />} />
+            <Route path="/edit/:id" element={<EditTask />} />
+            <Route path="/detail/:id" element={<TaskDetail />} />
+            <Route path="/usereducer" element={<UseReducerExample />} />
+            <Route path="/simple-update" element={<SimpleUpdate />} />
+            <Route path="/use-callback" element={<Search />} />
+            <Route path="/windowsizeteller" element={<WindowDimensions />} />
+            <Route
+              path="/Sent-to-parent"
+              element={<SendParent color={'Red'} getTitle={getTitle} />}
+            />
+            {/* <Route path="/buggy-component" element={<BuggyComponent />} /> */}
+            <Route path="/buggy-component" 
+              element={
+                <ErrorBoundary>
+                  <BuggyComponent />
+                </ErrorBoundary>
+              } 
+            />
+            <Route path="/demo-counter" element={<DemoCounter />} />
+          </Route>
+        </Routes>
+    </>
   );
 }
 
--- src/store/store.js
+++ src/store/store.js
@@ -1,10 +1,11 @@
-import { configureStore, createAsyncThunk } from '@reduxjs/toolkit';
+// import { configureStore, createAsyncThunk } from '@reduxjs/toolkit';
+import { configureStore } from '@reduxjs/toolkit';
 import rootReducers from './reducers';
 // Custom middleware
 const loggerMiddleware = (store) => (next) => (action) => {
-  console.log("Dispatching:", action);
+  //console.log("Dispatching:", action);
   const result = next(action);
-  console.log("Next State:", store.getState());
+  //console.log("Next State:", store.getState());
   return result;
 };
 // Async action
