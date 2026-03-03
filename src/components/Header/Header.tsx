5c8225e09368febcfc6f580ef3f1b1690b17c7ac
manoj30dec <manoj.kumar@nagarro.com>
2026-03-03 11:05:21 +0530

    replaced navigation

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
 import useAuth from '../../context/useAuth';
 function Header() {
   const [menu, setMenu] = useState(true);
@@ -12,167 +12,120 @@ function Header() {
     setMenu(false);
   };
   const { logout } = useAuth();
+  const [className, setClassName] = useState(false)
+  const toggleClass = ()=>{
+    className ? setClassName(false) : setClassName(true)
+  }
   return (
     <>
       <header className="border-b font-[sans-serif] tracking-wide relative z-50">
-        <section className="py-3 bg-[#1d294f] text-white text-center px-10 d-flex justify-between">
-          <p className="text-sm ">
+        <section className="py-3 bg-[#1d294f] text-white text-center px-10 d-flex justify-between align-middle">
+          <p className="text-sm">
             Task Management | Taks View, Task Details, and mark
           </p>
           <button className='btn btn-secondary'  onClick={logout}>Logout</button>
         </section>
-
-        <div className="flex flex-wrap items-center gap-4 px-10 py-4 relative bg-white min-h-[70px]">
-          <Link to="/" className="hidden max-lg:block">
-            <img
-              src="https://readymadeui.com/readymadeui.svg"
-              alt="logo"
-              className="w-36"
-            />
-          </Link>
-          {menu && (
-            <div
-              id="collapseMenu"
-              className="w-full max-lg:hidden lg:!block max-lg:fixed max-lg:before:fixed max-lg:before:bg-black max-lg:before:opacity-50 max-lg:before:inset-0 max-lg:before:z-50"
-            >
-              <button
-                id="toggleClose"
-                onClick={toggleClose}
-                className="lg:hidden fixed top-2 right-4 z-[100] rounded-full bg-white w-9 h-9 flex items-center justify-center border"
-              >
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  className="w-3.5 h-3.5 fill-black"
-                  viewBox="0 0 320.591 320.591"
+      </header>
+      <nav className="navbar navbar-expand-lg bg-body-tertiary">
+        <div className="container-fluid">
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarNavDropdown"
+            aria-controls="navbarNavDropdown"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon" />
+          </button>
+          <div className="collapse navbar-collapse" id="navbarNavDropdown">
+            <ul className="navbar-nav">
+              <li className="nav-item">
+                <NavLink to={'dashboard'} 
+                className={({ isActive }) =>
+                    isActive ? 'nav-link navbar-brand' : 'nav-link'
+                  }
+                aria-current="page" >
+                  Home
+                </NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink to={'/usereducer'} className={({ isActive }) =>
+                    isActive ? 'nav-link navbar-brand' : 'nav-link'
+                  } >
+                  Use Reducer
+                </NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink to={'/simple-update'} className={({ isActive }) =>
+                    isActive ? 'nav-link navbar-brand' : 'nav-link'
+                  } >
+                  Use Memo / Use Callback 
+                </NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink to={'/use-callback'} className={({ isActive }) =>
+                    isActive ? 'nav-link navbar-brand' : 'nav-link'
+                  } >
+                  Use Callback with search
+                </NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink to={'/windowsizeteller'} className={({ isActive }) =>
+                    isActive ? 'nav-link navbar-brand' : 'nav-link'
+                  } >
+                  Windowsize
+                </NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink to={'/up-lifting'} className={({ isActive }) =>
+                    isActive ? 'nav-link navbar-brand' : 'nav-link'
+                  } >
+                  Up lifting
+                </NavLink>
+              </li>
+             
+              <li className="nav-item">
+                <NavLink to={'/demo-redux-counter'} className={({ isActive }) =>
+                    isActive ? 'nav-link navbar-brand' : 'nav-link'
+                  } >
+                  Redux With Middleware
+                </NavLink>
+              </li>
+              <li className="nav-item dropdown">
+                <span
+                  className={`nav-link dropdown-toggle ${className ? 'show':''}`}
+                  role="button"
+                  data-bs-toggle="dropdown"
+                  aria-expanded="false"
+                  onClick={toggleClass}
                 >
-                  <path
-                    d="M30.391 318.583a30.37 30.37 0 0 1-21.56-7.288c-11.774-11.844-11.774-30.973 0-42.817L266.643 10.665c12.246-11.459 31.462-10.822 42.921 1.424 10.362 11.074 10.966 28.095 1.414 39.875L51.647 311.295a30.366 30.366 0 0 1-21.256 7.288z"
-                    data-original="#000000"
-                  ></path>
-                  <path
-                    d="M287.9 318.583a30.37 30.37 0 0 1-21.257-8.806L8.83 51.963C-2.078 39.225-.595 20.055 12.143 9.146c11.369-9.736 28.136-9.736 39.504 0l259.331 257.813c12.243 11.462 12.876 30.679 1.414 42.922-.456.487-.927.958-1.414 1.414a30.368 30.368 0 0 1-23.078 7.288z"
-                    data-original="#000000"
-                  ></path>
-                </svg>
-              </button>
-
-              <div className="lg:flex max-lg:fixed max-lg:bg-white max-lg:w-1/2 max-lg:min-w-[300px] max-lg:top-0 max-lg:left-0 max-lg:p-6 max-lg:h-full max-lg:shadow-md max-lg:overflow-auto z-50">
-                <ul className="lg:flex gap-4 ">
-                  <li className="mb-6 hidden max-lg:block">
-                    <Link to="#">
-                      <img
-                        src="https://readymadeui.com/readymadeui.svg"
-                        alt="logo"
-                        className="w-36"
-                      />
-                    </Link>
+                  More Demo
+                </span>
+                <ul className={`dropdown-menu ${className ? 'show':''}`}>
+                  <li onClick={()=>setClassName(false)}>
+                    <NavLink to={'/demo-custom-hook'} className="dropdown-item" >
+                      Custom Hook
+                    </NavLink>
                   </li>
-                  <li className="max-lg:border-b max-lg:py-3">
-                    <Link
-                      to="/"
-                      className="hover:text-[#007bff] font-semibold text-[#007bff] block text-[15px]"
-                    >
-                      Home
-                    </Link>
+                  <li onClick={()=>setClassName(false)}>
+                    <NavLink to={'/demo-infinite-scrolling'} className="dropdown-item" >
+                      Infinite Scrolling
+                    </NavLink>
                   </li>
-                  <li className="max-lg:border-b max-lg:py-3">
-                    <Link
-                      to={'/usereducer'}
-                      className="hover:text-[#007bff] font-semibold text-[#007bff] block text-[15px]"
-                    >
-                      Use Reducer Example
-                    </Link>
-                  </li>
-                  <li className="max-lg:border-b max-lg:py-3">
-                    <Link
-                      to={'/simple-update'}
-                      className="hover:text-[#007bff] font-semibold text-[#007bff] block text-[15px]"
-                    >
-                      Use Memo / Use Callback 
-                    </Link>
-                  </li>
-                  <li className="max-lg:border-b max-lg:py-3">
-                    <Link
-                      to={'/use-callback'}
-                      className="hover:text-[#007bff] font-semibold text-[#007bff] block text-[15px]"
-                    >
-                      Use Callback with search
-                    </Link>
-                  </li>
-                  <li className="max-lg:border-b max-lg:py-3">
-                    <Link
-                      to={'/windowsizeteller'}
-                      className="hover:text-[#007bff] font-semibold text-[#007bff] block text-[15px]"
-                    >
-                      Windowsize Teller
-                    </Link>
-                  </li>
-                  <li className="max-lg:border-b max-lg:py-3">
-                    <Link
-                      to={'/up-lifting'}
-                      className="hover:text-[#007bff] font-semibold text-[#007bff] block text-[15px]"
-                    >
-                      Up lifting
-                    </Link>
-                  </li>
-                  <li className="max-lg:border-b max-lg:py-3">
-                    <Link
-                      to={'/buggy-component'}
-                      className="hover:text-[#007bff] font-semibold text-[#007bff] block text-[15px]"
-                    >
-                      Error Boundary Example
-                    </Link>
-                    
-                  </li>
-                  <li className="max-lg:border-b max-lg:py-3">
-                    <Link
-                      to={'/demo-redux-counter'}
-                      className="hover:text-[#007bff] font-semibold text-[#007bff] block text-[15px]"
-                    >
-                      Demo Redux Middleware With Counter
-                    </Link>
-                  </li>
-                  <li className="max-lg:border-b max-lg:py-3">
-                    <Link
-                      to={'/demo-custom-hook'}
-                      className="hover:text-[#007bff] font-semibold text-[#007bff] block text-[15px]"
-                    >
-                      Demo Custom Hook
-                    </Link>
-                  </li>
-                  
-                  
-                  <li className="max-lg:border-b max-lg:py-3">
-                    <Link
-                      to={'/demo-infinite-scrolling'}
-                      className="hover:text-[#007bff] font-semibold text-[#007bff] block text-[15px]"
-                    >
-                      Demo Infinite Scrolling
-                    </Link>
+                  <li onClick={()=>setClassName(false)}>
+                    <NavLink to={'/buggy-component'} className="dropdown-item" >
+                      Error Boundary
+                    </NavLink>
                   </li>
                 </ul>
-              </div>
-            </div>
-          )}
-
-          <div className="flex ml-auto lg:hidden">
-            <button id="toggleOpen" onClick={toggleOpen}>
-              <svg
-                className="w-7 h-7"
-                fill="#000"
-                viewBox="0 0 20 20"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  fillRule="evenodd"
-                  d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
-                  clipRule="evenodd"
-                ></path>
-              </svg>
-            </button>
+              </li>
+            </ul>
           </div>
         </div>
-      </header>
+      </nav>
+
     </>
   );
 }
